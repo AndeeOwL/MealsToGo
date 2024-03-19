132b485773fa2a4167dc2fd912d729fcfb1d91e9
AndreanBuhchev <andy.buhchev@gmail.com>
2024-03-19 21:30:24 +0200

    setup vscode

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
-
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
-
-        return (
-    <>
-              <SafeAreaView style={{ flex: 1,marginTop: StatusBar.currentHeight}}>
-            <View style={styles.topContainer}>
-              <Text>search</Text>
-            </View>
-          <View style={styles.bottomContainer}>
-            <Text>list</Text>
-          </View>
-                  </SafeAreaView>
-    </>
-  );    
+  return (
+    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <View style={styles.topContainer}>
+        <Text>search</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   topContainer: {
-    backgroundColor: 'green',
-    padding: 16
+    backgroundColor: "green",
+    padding: 16,
   },
   bottomContainer: {
     flex: 1,
-    backgroundColor: 'blue',
-    padding: 16
-  }
+    backgroundColor: "blue",
+    padding: 16,
+  },
 });
